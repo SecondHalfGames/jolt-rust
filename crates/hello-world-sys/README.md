6117077f705a0a5a8d37ddef3ea5d29d23735c9b
Lucien Greathouse <me@lpghatguy.com>
2024-04-17 18:48:48 -0400

    Allocator API

---
+++ crates/hello-world-sys/README.md
@@ -0,0 +1,2 @@
+# Hello World (jolt-sys)
+A port of the Jolt Physics "HelloWorld" sample using `jolt-sys`.
--- crates/hello-world-sys/src/main.rs
+++ crates/hello-world-sys/src/main.rs
@@ -2,5 +2,12 @@ fn main() {
     unsafe {
         jolt_sys::JPC_RegisterDefaultAllocator();
         jolt_sys::JPC_FactoryInit();
+        jolt_sys::JPC_RegisterTypes();
+
+        let temp_allocator = jolt_sys::JPC_TempAllocatorImpl_new(10 * 1024 * 1024);
+
+        jolt_sys::JPC_TempAllocatorImpl_delete(temp_allocator);
     }
+
+    println!("Hello, world!");
 }
