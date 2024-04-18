80a3725dae59b23c8987d1efd0102211a87c6b7a
Lucien Greathouse <me@lpghatguy.com>
2024-04-17 20:49:53 -0400

    New types

--- crates/hello-world-sys/src/main.rs
+++ crates/hello-world-sys/src/main.rs
@@ -6,6 +6,15 @@ fn main() {
 
         let temp_allocator = jolt_sys::JPC_TempAllocatorImpl_new(10 * 1024 * 1024);
 
+        let job_system = jolt_sys::JPC_JobSystemThreadPool_new2(
+            jolt_sys::JPC_MAX_PHYSICS_JOBS as _,
+            jolt_sys::JPC_MAX_PHYSICS_BARRIERS as _,
+        );
+
+        let physics_system = jolt_sys::JPC_PhysicsSystem_new();
+
+        jolt_sys::JPC_PhysicsSystem_delete(physics_system);
+        jolt_sys::JPC_JobSystemThreadPool_delete(job_system);
         jolt_sys::JPC_TempAllocatorImpl_delete(temp_allocator);
     }
 
