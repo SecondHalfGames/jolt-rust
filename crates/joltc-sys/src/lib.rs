c3b56aa9f116c0b1293818881c1917b377db70d2
Lucien Greathouse <me@lpghatguy.com>
2024-04-26 00:08:18 -0400

    Macro for Default implementation, update JoltC

--- crates/joltc-sys/src/lib.rs
+++ crates/joltc-sys/src/lib.rs
@@ -8,12 +8,23 @@ pub type Real = f64;
 #[cfg(not(feature = "double-precision"))]
 pub type Real = f32;
 
-impl Default for JPC_BodyCreationSettings {
-    fn default() -> Self {
-        unsafe {
-            let mut settings = std::mem::MaybeUninit::<JPC_BodyCreationSettings>::zeroed();
-            JPC_BodyCreationSettings_default(settings.as_mut_ptr());
-            settings.assume_init()
-        }
-    }
+macro_rules! ffi_default {
+    ($($c_struct:ident -> $default_fn:ident,)*) => {
+        $(
+            impl Default for $c_struct {
+                fn default() -> Self {
+                    unsafe {
+                        let mut settings = std::mem::MaybeUninit::<$c_struct>::zeroed();
+                        $default_fn(settings.as_mut_ptr());
+                        settings.assume_init()
+                    }
+                }
+            }
+        )*
+    };
+}
+
+ffi_default! {
+    JPC_BodyCreationSettings -> JPC_BodyCreationSettings_default,
+    JPC_BoxShapeSettings2 -> JPC_BoxShapeSettings2_default,
 }
